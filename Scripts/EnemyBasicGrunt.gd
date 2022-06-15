b0bb58e24df485b86c89e438f30a46916d3093e2
Alex Burleigh <alebur33@kingshigh.school.nz>
2022-06-16 10:55:19 +1200

    Finished Video #7

--- Scripts/EnemyBasicGrunt.gd
+++ Scripts/EnemyBasicGrunt.gd
@@ -7,6 +7,7 @@ var path = [] #hold the path coordinates from the enemy to the player
 var path_index = 0 #keep track of which coordinate to go to
 var speed = 3
 var health = 20
+var move = true
 
 
 func _ready():
@@ -16,6 +17,12 @@ func take_damage(dmg_amount):
 	health -= dmg_amount
 	if health <= 0:
 		death()
+		return
+	move = false
+	$AnimatedSprite3D.play("hit")
+	yield($AnimatedSprite3D,"animation_finished")
+	$AnimatedSprite3D.play("walking")
+	move = true
 
 func _physics_process(delta):
 	if path_index < path.size():
@@ -23,7 +30,8 @@ func _physics_process(delta):
 		if direction.length() < 1:
 			path_index += 1
 		else:
-			move_and_slide(direction.normalized() * speed, Vector3.UP)
+			if move:
+				move_and_slide(direction.normalized() * speed, Vector3.UP)
 	else:
 		find_path(player.global_transform.origin)
 
@@ -36,7 +44,7 @@ func death():
 	set_physics_process(false)
 	$CollisionShape.disabled = true
 	if health <20:
-		$AnimationSprite3D.play("explode")
+		$AnimatedSprite3D.play("explode")
 	else:
 		$AnimatedSprite3D.play("die")
 
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,7 +7,10 @@ var max_speed = 8
 var mouse_sensitivity = 0.002
 
 #gun variables
-
+onready var pistol = preload("res://Scenes/Pistol.tscn")
+onready var shotgun =  preload("res://Scenes/Shotgun.tscn")
+var current_gun = 0
+onready var carried_guns = [pistol,shotgun]
 
 #functions
 
@@ -46,7 +49,18 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector3.UP, true)
 
 func change_gun(gun):
-	pass
+	$Pivot/Gun.get_child(0).queue_free()
+	var new_gun = carried_guns[gun].instance()
+	$Pivot/Gun.add_child(new_gun)
 
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("next_gun"):
+		current_gun+=1
+		if current_gun > len(carried_guns)-1:
+			current_gun = 0
+		change_gun(current_gun)
+	elif Input.is_action_just_pressed("prev_gun"):
+		current_gun -=1
+		if current_gun < 0:
+			current_gun = len(carried_guns)-1
+		change_gun(current_gun)
