c7d5e3bf23447c2af366172b8b02323c9f4443ef
Alex Burleigh <alebur33@kingshigh.school.nz>
2022-06-17 12:10:48 +1200

    Video #8 Done

--- Scripts/EnemyBasicGrunt.gd
+++ Scripts/EnemyBasicGrunt.gd
@@ -21,7 +21,7 @@ func take_damage(dmg_amount):
 	move = false
 	$AnimatedSprite3D.play("hit")
 	yield($AnimatedSprite3D,"animation_finished")
-	$AnimatedSprite3D.play("walking")
+	#$AnimatedSprite3D.play("walking")
 	move = true
 
 func _physics_process(delta):
@@ -31,6 +31,7 @@ func _physics_process(delta):
 			path_index += 1
 		else:
 			if move:
+				$AnimatedSprite3D.play("walking")
 				move_and_slide(direction.normalized() * speed, Vector3.UP)
 	else:
 		find_path(player.global_transform.origin)
